32cd8e1a262c959b9d7d3a76d4b2dedf5eccb454
rabinp <poudelrabin2004@example.com>
2025-02-08 20:31:21 +0545

    api execution complete

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,12 @@
 import React from "react";
 import Navbar from "../components/Navbar";
-import "@fortawesome/fontawesome-free/css/all.min.css";
 
 const Home = () => {
   return (
     <>
       {/* <Navbar /> added to main layout */}
-      <div>homeljkahsdfkjdsf</div>
-      <div className="text-center xl pt-5">
-        <i className="fa-solid fa-spinner fa-spin-pulse text-blue-500 text-2xl"></i>
-      </div>
+      <div>Home</div>
+      
     </>
   );
 };
