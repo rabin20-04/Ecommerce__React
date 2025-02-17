ede52eaf613295e2f5b5d563b76524ea5bd195e7
rabinp <poudelrabin2004@example.com>
2025-02-18 00:31:01 +0545

    path list

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import Navbar from "../components/Navbar";
-import List from "../Products/list";
+import List from "../products/List"
 
 const Products = () => {
   return (
