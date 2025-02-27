92e9005060b251bd5dfe9e61736df68b77dfc86f
rabin20-04 <poudelrabin2004@gmail.com>
2025-02-27 15:12:15 +0545

    home import

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { FaGithub, FaHome, FaLinkedin } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 const About = () => {
